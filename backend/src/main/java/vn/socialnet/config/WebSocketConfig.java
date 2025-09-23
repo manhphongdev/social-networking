60fa68475b6a2002a56b8ccfc34aab9b909e9931
manhphong <tranmanhphong2002@gmail.com>
2025-09-23 23:28:00 +0700

    Delete frontend directory

---
+++ backend/src/main/java/vn/socialnet/config/WebSocketConfig.java
@@ -0,0 +1,4 @@
+package vn.socialnet.config;
+
+public class WebSocketConfig {
+}
--- frontend/app/admin/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type React from "react"
-export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <div className="dark">{children}</div>
-}
